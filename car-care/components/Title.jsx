26f0c8bcce3adc75acbd3577a85739b1452cc3bd
OrkVili <n7n1mg@uni-sopron.hu>
2025-12-03 20:24:25 +0100

    Update styling

--- car-care/components/Title.jsx
+++ car-care/components/Title.jsx
@@ -1,17 +1,22 @@
 import styled from "styled-components"
 
 const Title = styled.h1`
+    text-align: center;
     font-size: 2.7rem;
     color: rgb(64, 224, 208);
     filter: drop-shadow(3px 2px 30px rgb(64, 224, 208));
     @media only screen and (max-width: 1000px) {
         font-size: 2rem;
-  }
+    }
 `
 
-export const SmallTitle = styled.h3`
+export const SmallTitle = styled.h2`
     color: rgb(64, 224, 208);
     filter: drop-shadow(3px 2px 30px rgb(64, 224, 208));
+    margin-bottom: 2rem;
+     @media only screen and (max-width: 1000px) {
+        font-size: 1.2rem;
+  }
 `
 
 export default Title
