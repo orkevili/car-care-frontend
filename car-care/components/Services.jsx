08e244174014bc9cf8cb77f32ed91c60dcfebc0f
OrkVili <n7n1mg@uni-sopron.hu>
2025-12-03 17:49:48 +0100

    Update Services page

--- car-care/components/Services.jsx
+++ car-care/components/Services.jsx
@@ -6,22 +6,27 @@ import Title from "./Title";
 import styled from "styled-components";
 import { useRoute } from "wouter";
 import Loader from "./Loading";
+import { FiEdit, FiPlus, FiTrash } from "react-icons/fi";
+import { toast } from "react-toastify";
+import { ActionButton, ActionModalContent, ButtonGroup, CancelButton, ModalContent, ModalInput, ModalOverlay, ModalTitle } from "./Modal";
 
 const Table = styled.table`
     margin: 1rem;
     background-color: rgba(47, 116, 109, 1);
     border-radius: 6px;
-    width: 95%; /* Teljesebb szélesség */
+    width: 90%;
+    & > thead td {
+        color: hsla(174, 92%, 71%, 1.00);
+        border-bottom: 2px solid rgba(20, 97, 97, 1);
+        font-weight: bold;
+    }
 `;
 
 const Tr = styled.tr`
     height: 3rem;
-    &:first-child > td { 
-        border-bottom: 2px solid rgba(20, 97, 97, 1);
-        font-weight: bold;
-    }
     &:hover {
-        background-color: rgba(47, 116, 109, 0.8);
+        background-color: rgba(9, 139, 126, 0.8);
+        box-shadow: 2px 5px 200px black;
     }
 `;
 
@@ -29,77 +34,152 @@ const Td = styled.td`
     width: fit-content;
     text-align: center;
     padding: 0.5rem;
-    color: white; /* Hogy látszódjon a szöveg */
+    color: white;
 `;
 
+
 function Services() {
-    const [serviceEvents, setServiceEvents] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [vehicleMap, setVehicleMap] = useState({});     
-    const [match, params] = useRoute("/services/:vehicleId");
-    const vehicleId = params ? params.vehicleId : null;
+    const [services, setServices] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [vehicleMap, setVehicleMap] = useState({})
+    const [match, params] = useRoute("/services/:vehicleId")
+    const [showEditModal, setShowEditModal] = useState(false)
+    const [showActionModal, setShowActionModal] = useState(false)
+    const [vehicleId, setVehicleId] = useState(null)
+
+    const [formData, setFormData] = useState({
+        id: null,
+        name: "",
+        odometer: "",
+        date: "",
+        cost: "",
+        description: "",
+        vehicle_id: ""
+    })
+
     
-    const pageTitle = vehicleId 
-        ? `Jármű Szerviztörténet (${vehicleMap[vehicleId] || 'ID: ' + vehicleId})` 
-        : "Összes szerviztörténet";
+    useEffect(() => {
+        setVehicleId(params ? params.vehicleId : null)
+    }, [params])
+    
+    const pageTitle = vehicleId ? `Service history ${vehicleMap[vehicleId]}` : "All service history"
+
+    const fetchAllData = async () => {
+        let serviceResponse;
+        try {
+            setLoading(true);                
+            const vehicleResponse = await VehicleAPI.getAll();
+            const rawVehicleData = vehicleResponse.data.Vehicles; 
+            const map = {};
+            if (rawVehicleData) {
+                const vehicleArray = Object.entries(rawVehicleData).map(([key, value]) => ({ id: key, ...value }));
+                vehicleArray.forEach(v => {
+                    map[v.id] = `${v.brand} ${v.model}`;
+                });
+            }
+            setVehicleMap(map);
+            
+            if (vehicleId) {
+                serviceResponse = await ServiceAPI.getById(vehicleId);
+            } else {
+                serviceResponse = await ServiceAPI.getAll(); 
+            }
+            
+            const rawServiceData = serviceResponse.data.Services; 
+            
+            if (rawServiceData) {
+                const servicesArray = Object.entries(rawServiceData).map(([key, value]) => ({
+                    id: key, 
+                    vehicle_id: String(value.vehicle_id), 
+                    ...value
+                }));
+                setServices(servicesArray);
+            } else {
+                setServices([]);
+            }
+        } catch (error) {
+            console.error("Error requesting data:", error);
+            toast.error("Couldn't load the data.")
+            setServices([]);
+            setVehicleMap({});
+        } finally {
+            setLoading(false);
+        }
+    };
 
     useEffect(() => {
-        
-        const fetchAllData = async () => {
-            let serviceResponse;
-            try {
-                setLoading(true);                
-                const vehicleResponse = await VehicleAPI.getAll();
-                const rawVehicleData = vehicleResponse.data.Vehicles; 
-                const map = {};
+        fetchAllData()
+    }, []); 
 
-                if (rawVehicleData) {
-                    const vehicleArray = Object.entries(rawVehicleData).map(([key, value]) => ({ id: key, ...value }));
-                    vehicleArray.forEach(v => {
-                        map[v.id] = `${v.brand} ${v.model}`;
-                    });
-                }
-                setVehicleMap(map);
-                
-                if (vehicleId) {
-                    serviceResponse = await ServiceAPI.getById(vehicleId);
-                } else {
-                    serviceResponse = await ServiceAPI.getAll(); 
-                }
-                
-                const rawServiceData = serviceResponse.data.Services; 
-                
-                if (rawServiceData) {
-                    const servicesArray = Object.entries(rawServiceData).map(([key, value]) => ({
-                        id: key, 
-                        vehicle_id: String(value.vehicle_id), 
-                        ...value
-                    }));
-                    setServiceEvents(servicesArray);
-                } else {
-                    setServiceEvents([]);
-                }
-            } catch (error) {
-                console.error("Hiba az adatok lekérésekor:", error);
-                setServiceEvents([]);
-                setVehicleMap({});
-            } finally {
-                setLoading(false);
+    const handleInputChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const resetForm = () => {
+        setFormData({id: null, name: "", odometer: "", date: "", cost: "", description: "", vehicle_id: ""})
+    }
+
+    const handleAddNew = () => {
+        resetForm()
+        setShowEditModal(true)
+    }
+
+    const handleEdit = (serviceData) => {
+        console.log(serviceData)
+        const editedData = {
+            ...serviceData,
+        }
+        setFormData(editedData)
+        setShowActionModal(false)
+        setShowEditModal(true)
+    }
+
+    const handleDelete = async (formData) => {
+        if (!window.confirm(`Are you sure to delete ${formData.name} service entry?`)) {
+            return;
+        }
+        try {
+            await ServiceAPI.delete(formData.id, vehicleId);
+            fetchAllData();
+            setShowActionModal(false);
+            resetForm();
+        } catch (error) {
+            console.error(error);
+                        toast.error(`Error during deleting ${formData.name} service entry.`)
+        }
+    };
+    
+    const handleSave = async () => {
+        if (!formData.name || !formData.odometer || !formData.date || !formData.cost || !formData.description) {
+            toast.warn("Name, odometer, date and cost are required!")
+            return
+        }
+        try {
+            if (formData.id) {
+                await ServiceAPI.update(formData.id, formData.vehicle_id, formData)
+            } else {
+                await ServiceAPI.create(vehicleId, formData)
             }
-        };
-
-        fetchAllData();
-    }, [vehicleId]); 
+            setShowEditModal(false)
+            resetForm()
+            fetchAllData()
+        } catch(error) {
+            alert("Error during save.")
+            console.error(error)
+        }
+    }
 
     const tableHeaders = [
-        `${!vehicleId ? "Jármű" : ""}`, "Esemény", "Km állás", "Dátum", "Költség", "Leírás"
+        `${!vehicleId ? "Vehicle" : ""}`, "Service", "Odometer", "Date", "Cost", "Description"
     ];
 
     return (
         <Container>
             <Title>{pageTitle}</Title>
-            <StyledButton>Add new</StyledButton>
-            
+            {vehicleId && <StyledButton onClick={handleAddNew}><FiPlus /></StyledButton>}
             {loading ? (
                 <Loader />
             ) : (
@@ -110,8 +190,8 @@ function Services() {
                         </Tr>
                     </thead>
                     <tbody>
-                        {serviceEvents.length > 0 ? (
-                            serviceEvents.map((event) => (
+                        {services.length > 0 ? (
+                            services.map((event) => (
                                 <Tr key={event.id}>
                                     {!vehicleId ? <Td>{vehicleMap[event.vehicle_id] || `ID: ${event.vehicle_id}`}</Td> : <Td></Td>}
                                     <Td>{event.name}</Td>
@@ -119,17 +199,77 @@ function Services() {
                                     <Td>{new Date(event.time).toLocaleDateString()}</Td>
                                     <Td>{event.cost} HUF</Td>
                                     <Td>{event.description}</Td>
+                                    <Td>
+                                        <ActionButton onClick={() => handleEdit(event)}><FiEdit /></ActionButton>
+                                        <ActionButton onClick={() => handleDelete(event)}><FiTrash /></ActionButton>
+                                        </Td>
                                 </Tr>
+                                
                             ))
                         ) : (
                             <tr>
-                                <Td colSpan={tableHeaders.length}>Nincs szervizbejegyzés.</Td>
+                                <Td colSpan={tableHeaders.length}>No service data</Td>
                             </tr>
                         )}
                     </tbody>
                 </Table>
             )}
-            
+            {showEditModal && (
+                <ModalOverlay onClick={() => setShowEditModal(false)}>
+                    <ModalContent onClick={(e) => e.stopPropagation()}>
+                        <ModalTitle>{formData.id ? `Edit service` : 'Add new service'}</ModalTitle>
+                        <ModalInput
+                            type="text"
+                            name="name"
+                            placeholder="Name"
+                            value={formData.name}
+                            onChange={handleInputChange}
+                        />
+                        <ModalInput
+                            type="text"
+                            name="description"
+                            placeholder="Description"
+                            value={formData.description}
+                            onChange={handleInputChange}
+                        />
+                         <ModalInput
+                            type="text"
+                            name="date"
+                            placeholder="Date"
+                            value={formData.date}
+                            onChange={handleInputChange}
+                        />
+                        <ModalInput
+                            type="number"
+                            step={100}
+                            name="odometer"
+                            placeholder="Odometer"
+                            value={formData.odometer}
+                            onChange={handleInputChange}
+                        />
+                        <ModalInput
+                            type="number"
+                            name="cost"
+                            step={1000}
+                            placeholder="Cost"
+                            value={formData.cost}
+                            onChange={handleInputChange}
+                        />
+                        <ModalInput 
+                            type="number"
+                            name="vehicle_id"
+                            defaultValue={formData.vehicle_id}
+                            hidden
+                        />
+                        <ButtonGroup>
+                            <CancelButton onClick={() => { setShowEditModal(false); resetForm() }}>Cancel</CancelButton>
+                            <StyledButton onClick={handleSave}>
+                                {formData.id ? 'Save changes' : 'Add'}
+                            </StyledButton>
+                        </ButtonGroup>
+                    </ModalContent>
+                </ModalOverlay>
+            )}
         </Container>
     );
 }
