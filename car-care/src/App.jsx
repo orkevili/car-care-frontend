84613850c50091b600109b9f26193222c16cccf6
OrkVili <n7n1mg@uni-sopron.hu>
2025-12-03 17:45:54 +0100

    Update App

--- car-care/src/App.jsx
+++ car-care/src/App.jsx
@@ -7,38 +7,63 @@ import Profile from '../components/Profile'
 import Services from '../components/Services'
 import Supplies from '../components/Supplies'
 import { AuthContext } from '../components/AuthContext'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import Loader from '../components/Loading'
+import { toast, ToastContainer } from 'react-toastify'
+import ErrorPage from '../components/ErrorPage'
+import { navigate } from 'wouter/use-browser-location'
 
 function App() {
-  const { user, loading, logout } = useContext(AuthContext)
+  const { logout, user, loading, serverDown } = useContext(AuthContext)
 
   if (loading) {
     return <Loader />
   }
 
+  if (serverDown) {
+    return <ErrorPage />
+  }
 
   return (
     <>
     {user && <Nav user={user} logout={logout} />} 
+    <ToastContainer
+      position="top-right"
+      autoClose={1000}
+      hideProgressBar={true}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark" 
+      toastStyle={{ 
+        backgroundColor: 'transparent', 
+        border: 'none',
+      }}
+    />
     <Switch>
     <Route path="/" >
       {user ? <Redirect to='/profile' /> : <Redirect to='/login' />}
     </Route>
     <Route path="/login">
-      <Auth />
+      {user ? <Redirect to='/profile' /> : <Auth />}
+    </Route>
+    <Route path="/register">
+      {user ? <Redirect to='/profile' /> : <Auth />}
     </Route>
     <Route path="/profile">
-      <Profile user={user} />
+      {user ? <Profile user={user} /> : <Redirect to='/login' />}
     </Route>
     <Route path="/garage">
-      <Garage />
+      {user ? <Garage /> : <Redirect to='/login' />}
     </Route>
     <Route path="/services/:vehicleId?">
       {user ? <Services /> : <Redirect to='/' />}
     </Route>
     <Route path="/supplies">
-      <Supplies />
+      {user ? <Supplies /> : <Redirect to='/login' />}
     </Route>
     </Switch>
     </>
