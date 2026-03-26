7bba7911675fcfc7ab699e3480042405340e88e0
Örkényi Vilmos <n7n1mg@uni-sopron.hu>
2026-03-26 11:07:31 +0100

    Update summary.jsx
    updated chartData, and chart

--- car-care/src/routes/Summary.jsx
+++ car-care/src/routes/Summary.jsx
@@ -8,23 +8,17 @@ export default function Summary() {
     const { services, parts, activeVehicle } = useContext(VehicleContext);
     const chartData = useMemo(() => {
         const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-        const data = months.map(m => ({ name: m, cost: 0, parts: 0 }));
+        const data = months.map(m => ({ name: m, cost: 0, part: 0 }));
         if (!services || services.length === 0) return data;
         services.forEach(service => {
             const date = new Date(service.date);
             const year = date.getFullYear();
             const monthIdx = date.getMonth();
 
-            data[monthIdx].cost += Number(service.labor_cost || 0);
-            service.used_parts && Array.isArray(service.used_parts) && service.used_parts.forEach((part) => {
-                const partInfo = parts.find(p => p.id === parseInt(part.part_id));
-                console.log(part)
-                //const quantity = service.used_parts.find(q => q.part_id === )
-                
-                data[monthIdx].parts += Number(partInfo.price)*Number(service.used_parts.quantity);
-
+            data[monthIdx].cost += Number(service.labor_cost);
+            service.used_parts && Array.isArray(service.used_parts) && service.used_parts.forEach((parts) => {
+                data[monthIdx].part += Number(parts.part_price)*Number(parts.quantity_used);
             })
-            
         });
         return data;
     }, [services, parts, activeVehicle]);
@@ -33,21 +27,23 @@ export default function Summary() {
     return (
         <Container>
             <Title>Summary</Title>
-            <SmallTitle>Ide majd jönnek valami fancy adatok</SmallTitle>
-            <BarChart 
-                style={{width: '70%', aspectRatio: 1}}
-                responsive
-                data={chartData}
-                margin={{top: 5, bottom: 5, left: 0, right: 0}}
-            >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis dataKey="cost" />
-            <Tooltip />
-            <Legend /> 
-            <Bar dataKey="cost" fill="#0aa5c0" activeBar={{fill: '#34b9a3', stroke: '#03caa9'}} radius={[10, 10, 0, 0]} />
-            <Bar dataKey="part" fill="#f80000" activeBar={{fill: '#34b9a3', stroke: 'green'}} radius={[10, 10, 0, 0]} />
-            </BarChart>
+            <SmallTitle>Service costs and parts</SmallTitle>
+            <ResponsiveContainer>
+                <BarChart 
+                    style={{ maxHeight: 600 }}
+                    responsive
+                    data={chartData}
+                    margin={{top: 30, bottom: 30, left: 0, right: 50}}
+                >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis dataKey="cost" />
+                <Tooltip />
+                <Legend /> 
+                <Bar dataKey="cost" fill="#09f0dc" activeBar={{fill: '#34b9a3', stroke: '#03caa9'}} radius={[10, 10, 0, 0]} />
+                <Bar dataKey="part" fill="#00ff62" activeBar={{fill: '#34b9a3', stroke: 'green'}} radius={[10, 10, 0, 0]} />
+                </BarChart>
+            </ResponsiveContainer>
         </Container>
     );
 }
