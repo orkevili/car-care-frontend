bd4f956a88af2ab3420b54c348aaab6780172597
OrkVili <orkenyi.vilmos@gmail.com>
2026-04-14 11:36:44 +0200

    Added proxy in config
    Updated vite.config.js proxy, for dev cors errors

--- car-care/vite.config.js
+++ car-care/vite.config.js
@@ -6,6 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    allowedHosts: ['localhost']
+    allowedHosts: ['localhost'],
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true
+      }
+    }
   }
 })
